0245f2c37bc180301d57bf39cd0a71550dd68786
Matthew Brett <matthew.brett@gmail.com>
2019-10-07 22:20:41 +0100

    Tell choose_python about 3.8 and above.

--- choose_python.sh
+++ choose_python.sh
@@ -5,11 +5,17 @@ py_ver=${PYTHON_VERSION:-3.5}
 uc_width=${UNICODE_WIDTH:-32}
 
 if [ "$py_ver" == "2.7" ] && [ "$uc_width" == "16" ] \
-    || [ "$py_ver" == "3.7" ]; then
+    || [ $(lex_ver ${py_ver}) -ge $(lex_ver 3.7) ]; then
     py_nodot=$(echo ${py_ver} | awk -F "." '{ print $1$2 }')
-    py_bin=/opt/cp${py_nodot}m/bin/python$py_ver
+    abi_suff=m
+    # Python 3.8 and up no longer uses the PYMALLOC 'm' suffix
+    # https://github.com/pypa/wheel/pull/303
+    if [ $(lex_ver ${py_ver}) -ge $(lex_ver 3.8) ]; then
+        abi_suff=""
+    fi
+    py_bin=/opt/cp${py_nodot}${abi_suff}/bin/python${py_ver}
 else
-    py_bin=/usr/bin/python$py_ver
+    py_bin=/usr/bin/python${py_ver}
 fi
 /root/.local/bin/virtualenv --python=$py_bin venv
 source venv/bin/activate
