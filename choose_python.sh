3a12e9f2d79cb63434f9ec5aca8320721e87e669
mattip <matti.picus@gmail.com>
2020-02-28 00:49:48 +0200

    fix path for python3.7, 3.8 when initializing image

--- choose_python.sh
+++ choose_python.sh
@@ -6,7 +6,7 @@ uc_width=${UNICODE_WIDTH:-32}
 
 py_nodot=$(echo ${py_ver} | awk -F "." '{ print $1$2 }')
 if [ "$py_ver" == "2.7" ] && [ "$uc_width" == "16" ] \
-    || [ ${py_nodot} -ge "37" ]; then
+    || [ ${py_nodot} -ge "39" ]; then
     abi_suff=m
     # Python 3.8 and up no longer uses the PYMALLOC 'm' suffix
     # https://github.com/pypa/wheel/pull/303
