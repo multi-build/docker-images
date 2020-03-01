6d70b3b411d13c850a8cdbc8f2a5c94970d506d4
mattip <matti.picus@gmail.com>
2020-03-01 23:40:03 +0200

    improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD https://hub.docker.com/
+set -ex
+
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 tag="multibuild/xenial_$PLATFORM"
 docker tag ${tag}:${TRAVIS_COMMIT} ${tag}:latest
 docker push ${tag}:latest
