76bd1280f0c367a31aca5b5d69cbba594f9a48b1
mattip <matti.picus@gmail.com>
2020-03-08 15:04:34 +0200

    do not depend on being able to import multibuild/common_utils.sh

--- docker/choose_python.sh
+++ docker/choose_python.sh
@@ -2,22 +2,81 @@
 # Choose python from PYTHON_VERSION, UNICODE_WIDTH
 # Then make a virtualenv from that Python and source it
 
-set -x
-
 py_ver=${PYTHON_VERSION:-3.5}
 uc_width=${UNICODE_WIDTH:-32}
+PYPY_URL=https://bitbucket.org/pypy/pypy/downloads
+DOWNLOADS_SDIR=downloads
+
+function install_pypy {
+    # Installs pypy.org PyPy
+    # Parameter $version
+    # Version given in major or major.minor or major.minor.micro e.g
+    # "3" or "3.7" or "3.7.1".
+    # Uses $PLAT
+    # sets $PYTHON_EXE variable to python executable
+
+    local version=$1
+    suffix=linux64
+    case "$PLAT" in
+    "x86_64")  suffix="linux64";;
+    "i686")    suffix="linux32";;
+    "darwin")  suffix="osx64";;
+    "ppc64le") suffix="ppc64le";;
+    "s30x")    suffix="s390x";;
+    "aarch64")  suffix="aarch64";;
+    *) if [ -n "$IS_OSX" ]; then
+            suffix="osx64";
+       else
+            echo unknown platform "$PLAT"; exit 1
+       fi;;
+    esac
+
+    # Need to convert pypy2.7-v7.2 to py_version=pypy2.7, pypy_ver=v7.2.0
+    local py_version=$(echo $version |cut -f1 -d'-')
+    local pypy_ver=$(echo $version |cut -f2 -d'-')
+    if [ "${pypy_ver:0:1}" == "v" ]; then
+        IFS='.' read -r -a array <<< "${pypy_ver:1}"
+    else
+        IFS='.' read -r -a array <<< "${pypy_ver}"
+    fi
+    major="${array[0]}"
+    minor="${array[1]-0}"
+    patch="${array[2]-0}"
+    local py_build=$py_version-v$major.$minor.$patch-$suffix
+    local py_zip=$py_build.tar.bz2
+    local zip_path=$DOWNLOADS_SDIR/$py_zip
+    mkdir -p $DOWNLOADS_SDIR
+    wget -nv $PYPY_URL/${py_zip} -P $DOWNLOADS_SDIR
+    tar -jxf $zip_path
+    # bug/feature: pypy package for pypy3 only has bin/pypy3 :(
+    if [ "${py_version:4:1}" == "3" ] && [ ! -x "$py_build/bin/pypy" ]; then
+        ln $py_build/bin/pypy3 $py_build/bin/pypy
+    fi
+    PYTHON_EXE=$(realpath $py_build/bin/pypy)
+    $PYTHON_EXE -mensurepip
+    PATH=$py_build/bin:$PATH
+    $PYTHON_EXE -mpip install --upgrade pip setuptools wheel
+    if [ "$major" == "3" ] && [ ! -x "$py_build/bin/pip" ]; then
+        ln $py_build/bin/pip3 $py_build/bin/pip
+    fi
+    PIP_CMD=pip
+}
+
 
 # The following also sets PYTHON_EXE and PIP_CMD
-if [ "${PYTHON_VERSION:0:4}" == "pypy" ]; then
-    source /io/common_utils.sh
+if [ "${py_ver:0:4}" == "pypy" ]; then
+    # Must be pre-installed, py_ver should be pypy3.6-7.3 or pypy2.7-7.3
     if [ -z "$PLAT" ]; then
-        PLAT="$(get_platform)"
+        # Use any Python that comes to hand.
+        PLAT="$(python -c 'import platform; print(platform.uname()[-1])')"
     fi
-    # TODO: pre-install some versions of PyPy.
-    pushd /io
-    install_pypy $PYTHON_VERSION
-    popd
+    install_pypy $py_ver
     py_bin=$PYTHON_EXE
+    impl="$($PYTHON_EXE -c 'import platform; print(platform.python_implementation())')"
+    if [ "$impl" != "PyPy" ]; then
+        echo failed to install PyPy
+        exit 1
+    fi
 else
     py_bin=/usr/bin/python${py_ver}
     if [ ! -e ${py_bin} ]; then
@@ -40,9 +99,6 @@ if [ ! -e ${py_bin} ]; then
     echo something wrong, "$py_bin" not found
     exit 1
 fi
-
-# the next step is too verbose with -x
-set +x
 
 /root/.local/bin/virtualenv --python=$py_bin venv
 source venv/bin/activate
