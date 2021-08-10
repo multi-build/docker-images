4506dc68d588798cf0b5ae9b658e1d67ed1a769f
mattip <matti.picus@gmail.com>
2021-08-10 10:37:55 +0300

    remove python2.7 narrow build, clean up build script

--- docker/build_install_pythons.sh
+++ docker/build_install_pythons.sh
@@ -32,60 +32,6 @@ BUILD_PKGS="zlib1g-dev libbz2-dev libncurses5-dev libreadline-gplv2-dev \
     liblzma-dev uuid-dev"
 apt-get -y install build-essential $BUILD_PKGS libffi-dev
 
-function compile_python {
-    local py_ver="$1"
-    local extra_args="$2"
-    local froot="Python-${py_ver}"
-    local ftgz="${froot}.tgz"
-    # Drop any suffix from three-digit version number
-    local py_nums=$(echo $py_ver |  awk -F "." '{printf "%d.%d.%d", $1, $2, $3}')
-    wget https://www.python.org/ftp/python/${py_nums}/${ftgz}
-    tar zxf ${ftgz}
-    local py_nodot=$(echo ${py_ver} | awk -F "." '{ print $1$2 }')
-    local abi_suff=m
-    # Python 3.8 and up no longer uses the PYMALLOC 'm' suffix
-    # https://github.com/pypa/wheel/pull/303
-    if [ ${py_nodot} -ge "38" ]; then
-        abi_suff=""
-    fi
-    local out_root=/opt/cp${py_nodot}${abi_suff}
-    mkdir $out_root
-    (cd Python-${py_ver} \
-        && ./configure --prefix=$out_root ${extra_args} \
-        && make \
-        && make install)
-    # Remove stray files
-    rm -rf ${froot} ${ftgz}
-}
-
-function build_openssl {
-    local version=$1
-    local froot="openssl-${version}"
-    local ftgz="${froot}.tar.gz"
-    wget https://www.openssl.org/source/${ftgz}
-    tar xvf ${ftgz}
-    (cd $froot &&
-    ./config no-ssl2 no-shared -fPIC --prefix=/usr/local/ssl &&
-    make &&
-    make install)
-    rm -rf ${froot} ${ftgz}
-}
-
-if [ 0 -eq 1 ]; then
-    # demo code to build a python version, not needed.
-    build_openssl 1.0.2o
-    for pyver in 3.7.6 3.8.2 ; do
-        compile_python $pyver "--with-openssl=/usr/local/ssl"
-    done
-fi
-
-
-# Compile narrow unicode Python
-# Compiled Pythons need to be flagged in the choose_python.sh script.
-compile_python 2.7.11 "--enable-unicode=ucs2"
-# Get pip for narrow unicode Python
-/opt/cp27m/bin/python get-pip-2.7.py
-
 # Clean out not-needed packages
 apt-get -y remove $BUILD_PKGS
 apt-get -y autoremove
