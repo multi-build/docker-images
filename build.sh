6aaf97cb10db1667c28937e768d7d01f2a033474
Stefan van der Walt <stefanv@berkeley.edu>
2021-10-13 22:10:27 +0000

    Use curly brackets for referring to shell variables

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 # Stop at any error, show all commands
 set -ex
 
-docker build --rm -t multibuild/$DIST_$PLATFORM:$TRAVIS_COMMIT -f docker/Dockerfile-$DIST-$PLATFORM docker/
+docker build --rm -t multibuild/${DIST}_${PLATFORM}:${TRAVIS_COMMIT} -f docker/Dockerfile-$DIST-$PLATFORM docker/
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ex
 
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-build="multibuild/$DIST_$PLATFORM"
+docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+build="multibuild/${DIST}_${PLATFORM}"
 docker tag ${build}:${TRAVIS_COMMIT} ${build}:latest
 docker push ${build}:${TRAVIS_COMMIT}
 docker push ${build}:latest
